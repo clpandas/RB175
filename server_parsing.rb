d0fb5cc460f7ba70d78e3a0e4ccd98fec68d1e3b
Crystal Lor <crystalalor@gmail.com>
2026-08-24 04:21:36 -0700

    Update server parsing

--- server_parsing.rb
+++ server_parsing.rb
@@ -3,6 +3,7 @@ require "socket"
 def parse_request(request_line)
   http_method, path_and_params, http = request_line.split(" ")
   path, params = path_and_params.split("?")
+  params ||= ""
   
   params = params.split("&").each_with_object({}) do |pair, hash|
     key, value = pair.split("=")
@@ -22,19 +23,30 @@ loop do
   http_method, path, params = parse_request(request_line)
 
   client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
-  client.puts request_line
+  client.puts "Content-Type: text/html"
+  # client.puts "Content-Type: text/plain\r\n\r\n"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  # client.puts request_line
   client.puts http_method
   client.puts path
   client.puts params
+  client.puts "</pre>"
+  
+  client.puts "<h1>Rolls!</h1>"
 
   rolls = params["rolls"].to_i
   sides = params["sides"].to_i
   
   rolls.times do
     roll = rand(sides) + 1
-    client.puts roll
+    client.puts "<p>", roll, "</p>"
   end
+
+  client.puts "</body>"
+  client.puts "</html>"
   
   client.close
 end
